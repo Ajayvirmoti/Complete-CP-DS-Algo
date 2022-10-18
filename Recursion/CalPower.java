3a969744901d094ef66512d3224bf796c08c71a4
Ajay Virmoti <73848796+Ajayvirmoti@users.noreply.github.com>
2022-10-19 01:27:48 +0530

    To calculate Power of given function

---
+++ Recursion/CalPower.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class CalPower {
+    public static int calPower(int x,int n){
+        if(x==0 ){
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+        int xpower1=calPower(x,n-1);
+        int xPown = x*xpower1;
+        return xPown;
+    }
+    public static void main(String[] args) {
+        int x=2, n=10;
+        int ans = calPower(x,n);
+        System.out.println(ans);
+    }
+}
