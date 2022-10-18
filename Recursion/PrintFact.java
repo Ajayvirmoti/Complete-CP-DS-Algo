c3f344e735001a347a4d6d06ac67456d921128c1
Ajay Virmoti <73848796+Ajayvirmoti@users.noreply.github.com>
2022-10-19 00:49:42 +0530

    Update PrintFact.java

--- Recursion/PrintFact.java
+++ Recursion/PrintFact.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class PrintFact {
     public static void printFact(int n,int fact){
+        // if n = 0 or we want to print factorial of 0
+        if (fact==1 && n==0){
+            System.out.println(1);
+            return;
+        }
         if(n==0){
             System.out.println(fact);
             return;
